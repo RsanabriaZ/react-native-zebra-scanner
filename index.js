31b1d9cb5fa726988dcee31346744b9ba30404cb
Rene Sanabria <rsanabria@hotmail.es>
2024-05-06 10:13:05 -0600

    fix removing listener

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const allowedEvents = [
   ZebraScanner.BARCODE_READ_FAIL,
 ];
 
+let listener = null;
+
 /**
  * Listen for available events
  * @param  {String} eventName Name of event one of barcodeReadSuccess, barcodeReadFail
@@ -16,7 +18,7 @@ const allowedEvents = [
   if (!allowedEvents.includes(eventName)) {
     throw new Error(`Event name ${eventName} is not a supported event.`);
   }
-  DeviceEventEmitter.addListener(eventName, handler);
+  listener = DeviceEventEmitter.addListener(eventName, handler);
 };
 
 /**
@@ -28,7 +30,10 @@ const allowedEvents = [
   if (!allowedEvents.includes(eventName)) {
     throw new Error(`Event name ${eventName} is not a supported event.`);
   }
-  DeviceEventEmitter.removeListener(eventName, handler);
+  
+  if (listener) {
+    listener.remove();
+  }
 };
 
 module.exports = ZebraScanner;
